262afe3e9726ec64cd6ac5eac1a19eb38e1ecbe9
João VIctor Silva <joaovictordes02@gmail.com>
2024-08-08 03:12:20 -0300

    Função de Criar conta

--- index.js
+++ index.js
@@ -28,7 +28,54 @@ function operation() {
     .then((answer) => {
       const action = answer["action"];
 
-      console.log(action);
+      if (action === "Criar conta") {
+        createAccount();
+      }
+    })
+    .catch((err) => console.log(err));
+}
+
+// Criando Conta
+
+function createAccount() {
+  console.log(chalk.bgGreen.black("Parabéns por escolher nosso banco!"));
+  console.log(chalk.green("Defina as opções da sua conta a seguir: "));
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Digite um nome para sua conta: ",
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+      console.info(accountName);
+
+      if (!fs.existsSync("accounts")) {
+        fs.mkdirSync("accounts");
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black("Está conta já existe, escolha outro nome.")
+        );
+        buildAccount();
+        return;
+      }
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance": 0}',
+        function (err) {
+          console.log(err);
+        }
+      );
+
+      console.log(chalk.green("Parabéns, sua conta foi criada com sucesso!"));
+      operation();
     })
     .catch((err) => console.log(err));
 }

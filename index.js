0904980b7031c29ee70ebfa4ebb30a9649c17905
João VIctor Silva <joaovictordes02@gmail.com>
2024-08-08 02:51:11 -0300

    Operações

--- index.js
+++ index.js
@@ -5,4 +5,30 @@ const chalk = require("chalk");
 // modulos internos
 const fs = require("fs");
 
-console.log("Setup inicial");
+// console.log("Setup inicial");
+
+operation();
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que voce deseja fazer?",
+        choices: [
+          "Criar conta",
+          "Consultar Saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer["action"];
+
+      console.log(action);
+    })
+    .catch((err) => console.log(err));
+}
